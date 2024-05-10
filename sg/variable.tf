49f3c2dd4626d610f7550409e9849090f5f95909
Jason Huynh <huynhvanvong2002@gmail.com>
2024-05-10 17:08:35 +0700

    modify sg module

--- sg/variable.tf
+++ sg/variable.tf
@@ -3,3 +3,45 @@ variable "name" {
   description = "Name of SG"
   default     = "demo-sg"
 }
+
+variable "ingress_rules" {
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  description = "List of ingress rules"
+  default = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "egress_rules" {
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  description = "List of egress rules"
+  default = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
